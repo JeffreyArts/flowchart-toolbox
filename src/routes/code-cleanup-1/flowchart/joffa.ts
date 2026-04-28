774b5c98f45a171a529b301989068493ccd2fba8
Jeffrey Arts <sjeffff@gmail.com>
2026-04-28 15:17:11 +0200

    Reset defaultDistance option

--- src/routes/code-cleanup-1/flowchart/joffa.ts
+++ src/routes/code-cleanup-1/flowchart/joffa.ts
@@ -27,7 +27,7 @@ export default class Joffa extends Flowchart {
         // Default tools
         this.register("tool","zoom", ZoomTool)
         this.register("tool","move-node", MoveNodeTool)
-        this.register("tool","add-node", AddNodeTool, { segments: 0, defaultDistance: 320 })
+        this.register("tool","add-node", AddNodeTool, { segments: 32 })
         this.register("tool","pan", PanTool)
         
         // Default Nodes
